6c781cf7041349cfdd7a8cf6921e2eabf8290568
adaiacamacho <wxlf.idk@gmail.com>
2025-12-01 19:17:35 +0100

    path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,37 +15,38 @@ public class Main {
             String com= sc.nextLine();
             switch (com) {
                 case "exit":
-                    sc.close();
-                    System.exit(0);
-                    break;
+                sc.close();
+                System.exit(0);
+                break;
                 case String a when a.startsWith("echo"):
-                    String b=a.substring(5);
-                    System.out.println(b);
-                    break; 
+                String b=a.substring(5);
+                System.out.println(b);
+                break; 
                 case String a when a.startsWith("type"):
-                    String x=a.substring(5);
-                    typeCase: {
-                        for(int i=0;i<types.length;i++){
-                            if(x.equals(types[i])){
-                                System.out.println(x.concat(" is a shell builtin"));
-                                break typeCase;
-                            }
+                String x=a.substring(5);
+                typeCase: {
+                    for(int i=0;i<types.length;i++){
+                        if(x.equals(types[i])){
+                            System.out.println(x.concat(" is a shell builtin"));
+                            break typeCase;
                         }
-                        String env= System.getenv("PATH");
-                        String[] paths = env != null ? env.split(File.pathSeparator) : new String[0];
-                        for (String path : paths) {
-                            File dirNatural=new File(path);
-                            File buscarDir=new File(dirNatural,x);
-                            if(buscarDir.exists() && buscarDir.canExecute()){
-                                System.out.println(x.concat(" is " + buscarDir));
-                                break typeCase;
-                            }
-                        }   
-                        System.out.println(x.concat(": not found"));
                     }
-                    break;
+                    String env= System.getenv("PATH");
+                    String[] paths = env != null ? env.split(File.pathSeparator) : new String[0];
+                    for (String path : paths) {
+                        File dirNatural=new File(path);
+                        File buscarDir=new File(dirNatural,x);
+                        if(buscarDir.exists() && buscarDir.canExecute()){
+                            System.out.println(x.concat(" is " + buscarDir));
+                            break typeCase;
+                        }
+                    }   
+                    System.out.println(x.concat(": not found"));
+                }
+                break;
                 default:
-                    tryExec:{
+                tryExec:{
+                    try {     
                         String exec="";
                         for(int i=0;i<10;i++){
                             String ch=com.valueOf(i);
@@ -59,16 +60,19 @@ public class Main {
                             File dirNatural=new File(path);
                             File buscarDir=new File(dirNatural,exec);
                             if(buscarDir.exists() && buscarDir.canExecute()){
-                               ProcessBuilder pb = new ProcessBuilder(Arrays.asList(com.split(" ")));
-                               pb.inheritIO();
-                               Process p = pb.start();
-                               p.waitFor();
-                               break tryExec;
+                                ProcessBuilder pb = new ProcessBuilder(Arrays.asList(com.split(" ")));
+                                pb.inheritIO();
+                                Process p = pb.start();
+                                p.waitFor();
+                                break tryExec;
                             }
-                        }   
+                        }  
+                    } catch (Exception e) {
                         System.out.println(com.concat(": command not found"));
-                    }
-                    break;
+                    } 
+                    
+                }
+                break;
             }
         }while(true);
         
