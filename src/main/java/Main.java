ac06f57d7c8deceb0fdfdd0c580303aa1b2ea13a
adaiacamacho <wxlf.idk@gmail.com>
2025-12-01 14:42:04 +0100

    repl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         //TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
-        Scanner sc=new Scanner(System.in);
-        String com= sc.nextLine();
-        if(!com.equals(" ")){
-            System.out.println(com.concat(": command not found"));
-        }
+        do{
+            System.out.print("$ ");
+            Scanner sc=new Scanner(System.in);
+            String com= sc.nextLine();
+            if(!com.equals(" ")){
+                System.out.println(com.concat(": command not found"));
+            }
+        }while(true);
+        
     }
 }
