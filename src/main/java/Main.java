7de43887218e0912be63663a2253ee60578ff4c6
adaiacamacho <wxlf.idk@gmail.com>
2025-12-04 17:27:28 +0100

    cd pt2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -58,8 +59,36 @@ public class Main {
                                 System.out.println(new String("cd: " + objetivo+": No such file or directory"));
                             }
                             break;
-                    
+                        case '.':
+                            String ob2=a.substring(3);
+                            if(ob2.startsWith("./")){
+                                String nuevo=ob2.substring(1);
+                                String previo=System.getProperty("user.dir");
+                                System.setProperty("user.dir", new String(previo+nuevo));
+                            }else if(ob2.startsWith("..")){
+                                String[] pasos=ob2.split("/");
+                                for(String p:pasos){
+                                    if(p.equals("..")){
+                                        File act=new File(System.getProperty("user.dir"));
+                                        Path menos=act.toPath();
+                                        Path nuevo=menos.subpath(0, menos.getNameCount()-1);
+                                        System.setProperty("user.dir", nuevo.toString());
+                                    }else{
+                                        String moveDown=System.getProperty("user.dir");
+                                        System.setProperty("user.dir", new String(moveDown+"/"+p));
+                                    }
+                                }
+                            }
+                            break;
                         default:
+                            String ob3=a.substring(3);
+                            File f1=new File(ob3);
+                            String prev=System.getProperty("user.dir");
+                            if(f1.exists()&&f1.isDirectory()&&f1.getAbsolutePath().contains(prev)){
+                                System.setProperty("user.dir", new String(prev+"/"+ob3));
+                            }else{
+                                System.out.println(new String("cd: " + ob3+": No such file or directory"));
+                            }
                             break;
                     }
                     break;
